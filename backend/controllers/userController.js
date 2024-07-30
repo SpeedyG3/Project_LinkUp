3fb13a198b92f6762eac83a45b719678d847886a
Hrishi <hrishikeshkulkarni009@gmail.com>
2024-07-30 17:56:42 +0530

    usersDoneforNow

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,20 @@ import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
 import generateTokenAndSetCookie from "../utils/helpers/genTokenAndSetCookie.js";
 
+const getUserProfile = async (req, res) => {
+    const {username} = req.params;
+    try{
+        const user = await User.findOne({username}).select("-password").select("-updatedAt");
+        if(!user){
+            return res.status(400).json({message: "User not found"});
+        }
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json({message: err.message});
+        console.log("Error in getUserProfile", err.message);
+    }
+};
+
 const signupUser = async (req, res) => {
     try{
         const {name, email, username, password} = req.body;
@@ -79,7 +93,7 @@ const followUnfollowUser = async(req, res) => {
         const userToUpdate = await User.findById(id);
         const currUser = await User.findById(req.user._id);
 
-        if(id==req.user._id){
+        if(id==req.user._id.toString()){
             return res.status(400).json({message: "You cannot follow/unfollow yourself"});
         }
 
@@ -105,6 +119,40 @@ const followUnfollowUser = async(req, res) => {
         res.status(500).json({message: err.message});
         console.log("Error in follow/unfollow user", err.message);
     }
-}
+};
 
-export {signupUser, loginUser, logoutUser, followUnfollowUser};
+const updateUser = async(req, res) => {
+    const {name, email, username, password, profilePic, bio} = req.body;
+    const userId = req.user._id;
+    try{
+        let user = await User.findById(userId);
+        if(!user){
+            return res.status(400).json({message: "User not found"});
+        }
+
+        if(req.params.id !== userId.toString()){
+            return res.status(400).json({message: "You cannot update other user's profile"});
+        }
+
+        if(password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+
+        user = await user.save();
+        res.status(200).json({message: "Profile updated successfully"});
+
+    }catch(err){
+        res.status(500).json({message: err.message});
+        console.log("Error in updateUser", err.message);
+    }
+};
+
+export {signupUser, loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import {signupUser, loginUser, logoutUser, followUnfollowUser} from '../controllers/userController.js';
+import {signupUser, loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile} from '../controllers/userController.js';
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-
 router.post("/follow/:id", protectRoute, followUnfollowUser); //unfollow included too
+router.post("/update/:id", protectRoute, updateUser);
+
 export default router; 
