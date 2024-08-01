7e6c15ce7dc5698eccb69bf8579c1a04f106b3e9
Hrishi <hrishikeshkulkarni009@gmail.com>
2024-08-01 13:13:38 +0530

    PushAbut

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 import UserPage from "./pages/UserPage";
 import Header from "./components/Header";
 import PostPage from "./pages/PostPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Container maxW="620px">
       <Header />
       <Routes>
+        <Route path="/" element={<HomePage/>}/>
         <Route path="/:username" element={<UserPage />}/>
         <Route path="/:username/post/:pid" element={<PostPage />}/>
       </Routes>
